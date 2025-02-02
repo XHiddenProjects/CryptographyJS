43a3f01fb01ef7a13bf23bc8877486f011378caa
Gavin Zeager <64079569+XHiddenProjects@users.noreply.github.com>
2025-02-02 13:07:23 -0500

    Update cryptography.js

--- cryptography.js
+++ cryptography.js
@@ -345,3 +345,22 @@ Cryptography.OneTimePad = {
         return decrypted;
     }
 };
+
+Cryptography.base64 = {
+    /**
+     * Encode string to Base64
+     * @param {String} str String to encode
+     * @returns {String} Encoded string
+     */
+    encode: (str) => {
+        return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, (match, p1) => String.fromCharCode('0x' + p1)));
+    },
+    /**
+     * Decode Base64 to string
+     * @param {String} str Base64 encoded string
+     * @returns {String} Decoded string
+     */
+    decode: (str) => {
+        return decodeURIComponent(atob(str).split('').map(c => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)).join(''));
+    }
+}
