604db0baf36bdde6c3b2b296d011c2f8177c4375
Gavin Zeager <64079569+XHiddenProjects@users.noreply.github.com>
2025-02-06 14:15:13 -0500

    Update ping.php

--- scripts/ping.php
+++ scripts/ping.php
@@ -1,6 +1,1 @@
-<?php
-header('Content-Type: application/json; charset=utf-8');
-if(isset($_REQUEST['ping'])){
-    echo json_encode(['host'=>gethostbyname(htmlentities(preg_replace('/https?:\/\//','',$_REQUEST['ping'])))],JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES);
-}
-?>
+<?php header('Content-Type: application/json; charset=utf-8');if(isset($_REQUEST['ping'])){echo json_encode(['host'=>gethostbyname(htmlentities(preg_replace('/https?:\/\//','',$_REQUEST['ping'])))],JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES);}?>
