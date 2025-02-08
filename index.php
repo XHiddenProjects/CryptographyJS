1a3112a77f46de57a633f9058854bf6d0f3cd18b
Gavin Zeager <64079569+XHiddenProjects@users.noreply.github.com>
2025-02-08 10:42:18 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Cyber</title>
+    </head>
+    <body>
+        <input type="text"/>
+
+        <script type="module">
+            import {Cryptography, Hacking, Detections} from './cyber.js';
+            
+            console.log(Hacking.xss.sanitize());
+        </script>
+    </body>
+</html>
